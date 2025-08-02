0e95c08b2d07d38505537b29aea7b641b9fbb5fe
Mihir Laud <mihirlaud@gmail.com>
2025-08-01 19:55:31 -0500

    Add Queen of Hearts level

---
+++ entities/npc/homing_head.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+@export var player: CharacterBody2D
+
+var SPEED = 500
+
+var randomnum
+
+enum {
+	ROAM
+}
+
+var state = ROAM
+
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	randomnum = rng.randf()
+	
+
+func _physics_process(delta):
+	match state:
+		ROAM:
+			move(get_circle_position(randomnum), delta)
+
+func move(target, delta):
+	var direction = (target - global_position).normalized() 
+	var desired_velocity =  direction * SPEED
+	var steering = (desired_velocity - velocity) * delta * 2.5
+	velocity += steering
+	move_and_slide()
+	
+func get_circle_position(random):
+	var kill_circle_centre = player.global_position
+	var radius = 40
+	 #Distance from center to circumference of circle
+	var angle = random * PI * 2;
+	var x = kill_circle_centre.x + cos(angle) * radius;
+	var y = kill_circle_centre.y + sin(angle) * radius;
+
+	return Vector2(x, y)
+
+func _on_timer_timeout():
+	self.queue_free()
---
+++ entities/npc/queen.gd
@@ -0,0 +1,59 @@
+extends CharacterBody2D
+
+@export var player: CharacterBody2D
+
+var SPEED = 200
+
+var rng
+var randomnum
+
+var head_scene
+
+enum {
+	ROAM,
+	STAY,
+}
+
+var state = STAY
+
+func _ready():
+	rng = RandomNumberGenerator.new()
+	rng.randomize()
+	randomnum = rng.randf()
+	
+	head_scene = preload("res://entities/npc/homing_head.tscn")
+
+func _physics_process(delta):
+	match state:
+		ROAM:
+			move(get_circle_position(randomnum), delta)
+		STAY:
+			pass
+
+func move(target, delta):
+	var direction = (target - global_position).normalized() 
+	var desired_velocity =  direction * SPEED
+	var steering = (desired_velocity - velocity) * delta * 2.5
+	velocity += steering
+	move_and_slide()
+	
+func get_circle_position(random):
+	var kill_circle_centre = player.global_position
+	var radius = 40
+	 #Distance from center to circumference of circle
+	var angle = random * PI * 2;
+	var x = kill_circle_centre.x + cos(angle) * radius;
+	var y = kill_circle_centre.y + sin(angle) * radius;
+
+	return Vector2(x, y)
+
+func launch_attack():
+	var attack = floor(rng.randf() * 4.0)
+	print(attack)
+	if attack == 0.0:
+		var head = head_scene.instantiate()
+		head.player = self.player
+		add_child(head)
+	else:
+		pass
+		

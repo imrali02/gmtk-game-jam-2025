5e727786b67844e84fa287b1cb395d821b85731e
Mihir Laud <mihirlaud@gmail.com>
2025-08-02 15:58:56 -0500

    Add damage to player and boss

--- entities/npc/queen-level/card.gd
+++ entities/npc/queen-level/card.gd
@@ -5,9 +5,14 @@ var face_up
 var card_back = preload("res://assets/card_back.png")
 var spade_face = preload("res://assets/card_face_spade.png")
 var heart_face = preload("res://assets/card_face_heart.png")
+var player_ref
 
 func _ready():
 	face_up = false
+	
+	var players = get_tree().get_nodes_in_group("player")
+	if players.size() > 0:
+		player_ref = players[0]
 	
 func _physics_process(delta):
 	$CardRewinder.handle_rewind()
@@ -17,9 +22,23 @@ func flip():
 	if face_up:
 		face_up = false
 		$Sprite2D2.texture = card_back
+		if is_heart:
+			$CollisionShape2D.set_deferred("disabled", true)
+		else:
+			$CollisionShape2D.set_deferred("disabled", false)
 	else:
 		face_up = true
+		$CollisionShape2D.set_deferred("disabled", false)
 		if is_heart:
 			$Sprite2D2.texture = heart_face
 		else:
 			$Sprite2D2.texture = spade_face
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		if face_up:
+			player_ref.take_damage(20.0)
+		else:
+			if not is_heart:
+				player_ref.take_damage(20.0)
--- entities/npc/queen-level/sword.gd
+++ entities/npc/queen-level/sword.gd
@@ -4,6 +4,8 @@ var SPEED = 4
 
 var randomnum
 var end
+var player_ref
+var swinging = false
 
 func _ready():
 	var rng = RandomNumberGenerator.new()
@@ -18,7 +20,14 @@ func _ready():
 		self.rotation = -PI/2
 		end = PI/2
 	
+	var players = get_tree().get_nodes_in_group("player")
+	if players.size() > 0:
+		player_ref = players[0]
+	
 func _physics_process(delta):
+	if not swinging:
+		return
+	
 	var new_angle
 	if end > 0:
 		new_angle = self.transform.get_rotation() + delta * SPEED
@@ -32,3 +41,13 @@ func _physics_process(delta):
 	elif end > 0 and self.rotation >= PI/2:
 		self.queue_free()
 		
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("player"):
+		if not player_ref.get_node("Rewinder").rewinding:
+			player_ref.take_damage(10.0)
+
+
+func _on_timer_timeout():
+	swinging = true
